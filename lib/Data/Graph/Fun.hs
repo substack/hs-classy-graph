021200a7cb6981b7a7232ab4ed93f3033291c747
James Halliday <substack@gmail.com>
2010-02-20 06:15:17 -0900

    very basic graph construction

---
+++ lib/Data/Graph/Fun.hs
@@ -0,0 +1,45 @@
+-- A graph module that takes care of all those pesky details,
+-- putting the fun back in functional!
+module Data.Graph.Fun (
+    FGraph(..), Edge, empty, fromEdges, addEdge, addNode
+) where
+
+import qualified Data.Map as M
+import qualified Data.Bimap as B
+import qualified Data.Set as S
+
+type NodeId = Int
+type NodeMap a = B.Bimap NodeId a
+type EdgeMap = M.Map NodeId (S.Set NodeId)
+type Edge a = (a,a)
+
+data FGraph a = FGraph {
+    nodeMap :: NodeMap a,
+    nextNodeId :: NodeId,
+    edgeMap :: EdgeMap
+} deriving Show
+
+empty :: Eq a => FGraph a
+empty = FGraph { nodeMap = B.empty, nextNodeId = 0, edgeMap = M.empty }
+
+fromEdges :: (Eq a, Ord a) => [Edge a] -> FGraph a
+fromEdges = foldl (flip addEdge) empty
+
+addEdge :: (Eq a, Ord a) => Edge a -> FGraph a -> FGraph a
+addEdge (n1,n2) g = g''
+    where
+        g' = addNode n2 $ addNode n1 g
+        g'' = g' { edgeMap = eM' }
+        eM = edgeMap g'
+        eM' = M.insertWith S.union idN1 (S.singleton idN2) eM
+        nM = nodeMap g'
+        idN1,idN2 :: NodeId
+        (idN1,idN2) = (nM B.!> n1, nM B.!> n2)
+
+addNode :: (Eq a, Ord a) => a -> FGraph a -> FGraph a
+addNode n g@FGraph{ nodeMap = nM, nextNodeId = nId } = g'
+    where
+        g' = g { nodeMap = nM', nextNodeId = nId' }
+        (nM',nId') = case B.lookupR n nM of
+            Nothing -> (B.insert nId n nM, nId + 1)
+            _ -> (nM, nId)
